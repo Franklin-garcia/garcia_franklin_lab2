2b930e0a91a4e663783666f5688fd09a0356660e
franklin garcia <franklin__garcia@unitec.edu>
2017-07-28 14:06:11 -0600

    agregando

--- Lab2_garcia_franklin/src/lab2_garcia_franklin/Lab2_garcia_franklin.java
+++ Lab2_garcia_franklin/src/lab2_garcia_franklin/Lab2_garcia_franklin.java
@@ -5,6 +5,9 @@
  */
 package lab2_garcia_franklin;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Franklin Garcia
@@ -16,6 +19,68 @@ public class Lab2_garcia_franklin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
+         crear usuarios, modificar los datos de un usuario, 
+        eliminar un usuario y también ingresar a la aplicación por medio de un login
+         */
+        ArrayList<Usuario> lista_general = new ArrayList();
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("5")) {
+            opcion = JOptionPane.showInputDialog("Seleccione opcion \n"
+                    + "1-Crear usuarios \n"
+                    + "2-Modificar Usuarios \n"
+                    + "3-Elimiminar usuarios \n"
+                    + "4-Login");
+            switch (opcion) {
+                case "1": {
+                    String nombre = JOptionPane.showInputDialog("Nombre");
+                    int edad = Integer.parseInt(JOptionPane.showInputDialog("Edad"));
+                    String lugar_nacimiento = JOptionPane.showInputDialog("lugar de nacimiento");
+                    String username = JOptionPane.showInputDialog("usuario");
+                    String password = JOptionPane.showInputDialog("contraseña");
+                    lista_general.add(new Usuario(nombre, edad, lugar_nacimiento, username, password));
+                    JOptionPane.showMessageDialog(null, "!Hecho¡");
+                }
+                break;
+                case "2": {
+                    String e = "";//
+                    for (Usuario t : lista_general) {
+                        e += "\n" + lista_general.indexOf(t) + " " + t;
+                    }
+                    JOptionPane.showMessageDialog(null, e);//
+
+                    int pos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese posicion"));
+                    String nombre = JOptionPane.showInputDialog("Nombre");
+                    int edad = Integer.parseInt(JOptionPane.showInputDialog("Edad"));
+                    String lugar_nacimiento = JOptionPane.showInputDialog("lugar de nacimiento");
+                    String username = JOptionPane.showInputDialog("usuario");
+                    String password = JOptionPane.showInputDialog("contraseña");
+                    lista_general.get(pos).setNombre(nombre);
+                    lista_general.get(pos).setEdad(edad);
+                    lista_general.get(pos).setLugar_nacimiento(lugar_nacimiento);
+                    lista_general.get(pos).setUsername(username);
+                    lista_general.get(pos).setPassword(password);
+                    JOptionPane.showMessageDialog(null, "!Hecho¡");
+                }
+                break;
+                case "3": {
+                    String e = "";//
+                    for (Usuario t : lista_general) {
+                        e += "\n" + lista_general.indexOf(t) + " " + t;
+                    }
+                    JOptionPane.showMessageDialog(null, e);//
+                    int pos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese posicion"));
+                    lista_general.remove(pos);
+                    JOptionPane.showMessageDialog(null, "!Hecho¡");
+                }
+                break;
+                case "4": {
+
+                }
+                break;
+            }
+        }
+
     }
-    
+
 }
--- Lab2_garcia_franklin/src/lab2_garcia_franklin/Usuario.java
+++ Lab2_garcia_franklin/src/lab2_garcia_franklin/Usuario.java
@@ -5,10 +5,115 @@
  */
 package lab2_garcia_franklin;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Franklin Garcia
  */
 public class Usuario {
+    private String nombre;
+    private int edad;
+    private String lugar_nacimiento;
+    private String username;
+    private String password;
+    private ArrayList<Usuario> lista_amigos; 
+    private ArrayList lista_solicitudes;
+    private ArrayList <String>lista_mensajes_enviados;
+    private ArrayList<String>buzón_entrada;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, int edad, String lugar_nacimiento, String username, String password) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.lugar_nacimiento = lugar_nacimiento;
+        this.username = username;
+        this.password = password;
+        this.lista_amigos = lista_amigos;
+        this.lista_solicitudes = lista_solicitudes;
+        this.lista_mensajes_enviados = lista_mensajes_enviados;
+        this.buzón_entrada = buzón_entrada;
+    }
+
+ 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLugar_nacimiento() {
+        return lugar_nacimiento;
+    }
+
+    public void setLugar_nacimiento(String lugar_nacimiento) {
+        this.lugar_nacimiento = lugar_nacimiento;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Usuario> getLista_amigos() {
+        return lista_amigos;
+    }
+
+    public void setLista_amigos(ArrayList<Usuario> lista_amigos) {
+        this.lista_amigos = lista_amigos;
+    }
+
+    public ArrayList getLista_solicitudes() {
+        return lista_solicitudes;
+    }
+
+    public void setLista_solicitudes(ArrayList lista_solicitudes) {
+        this.lista_solicitudes = lista_solicitudes;
+    }
+
+    public ArrayList<String> getLista_mensajes_enviados() {
+        return lista_mensajes_enviados;
+    }
+
+    public void setLista_mensajes_enviados(ArrayList<String> lista_mensajes_enviados) {
+        this.lista_mensajes_enviados = lista_mensajes_enviados;
+    }
+
+    public ArrayList<String> getBuzón_entrada() {
+        return buzón_entrada;
+    }
+
+    public void setBuzón_entrada(ArrayList<String> buzón_entrada) {
+        this.buzón_entrada = buzón_entrada;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", edad=" + edad + ", lugar_nacimiento=" + lugar_nacimiento + ", username=" + username + ", password=" + password + ", lista_amigos=" + lista_amigos + ", lista_solicitudes=" + lista_solicitudes + ", lista_mensajes_enviados=" + lista_mensajes_enviados + ", buz\u00f3n_entrada=" + buzón_entrada + '}';
+    }
     
 }
